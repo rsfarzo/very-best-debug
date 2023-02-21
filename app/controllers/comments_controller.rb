cfae0a7f9078b3c0d2e9d054d80ee218f81eb21f
Robert Sfarzo <sfarzo@uillinois.edu>
2023-02-21 02:05:00 +0000

    100% !!!!!!!!!!!!!

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
   def create
-    puts "saving new comment"
+    puts "saving new comment #{Comment.all.length} "
     comment = Comment.new
     #comment.author_id = params.fetch("query_author_id")
     comment.author_id = params.fetch("query_user_id") #uid")
     comment.venue_id = params.fetch("query_venue_id")
     comment.body = params.fetch("query_body")
     if (comment.save)
-      puts "saved"
+      puts "saved #{Comment.all.length}"
     else
       puts "error saving"
     end
